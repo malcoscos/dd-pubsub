85be9a3a65b463ec0950f98c65c9d6b8503a5b48
malcos <malcos@dp118-110.jaist.ac.jp>
2023-04-28 20:08:08 +0900

    sub server get data from nfs server by URL

---
+++ dd-pub.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func main() {
+
+	// optsにClientOptionsインスタンスのpointerを格納
+	opts := mqtt.NewClientOptions()
+	//　BrokerServerのlistに追加
+	opts.AddBroker("tcp://localhost:1883")
+	// clientクラスのインスタンスを作成
+	c := mqtt.NewClient(opts)
+	// BrokerへのconnectionにErrorがないか判定
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("Mqtt error: %s", token.Error())
+	}
+	// clientからosのシステムを利用してパケットをbrokerにstoreする
+	for i := 0; i < 5; i++ {
+		d1 := []byte("hello world")
+		file_name := fmt.Sprintf("/tmp/test%d.text", i)
+		err := os.WriteFile(file_name, d1, 0664)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		// text := fmt.Sprintf("this is msg #%d!", i)
+
+		token := c.Publish("go-mqtt/sample", 0, false, file_name)
+
+		token.Wait()
+	}
+
+	c.Disconnect(250)
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("Complete publish")
+}
+
+type data struct {
+	pub_data int
+}
---
+++ dd-sub.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func main() {
+	// channelの作成
+	msgCh := make(chan mqtt.Message)
+	// messageをchannelに送信する関数の作成
+	var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+		msgCh <- msg
+	}
+	// optsにClientOptionsインスタンスのpointerを格納
+	opts := mqtt.NewClientOptions()
+	//　BrokerServerのlistに追加
+	opts.AddBroker("tcp://localhost:1883")
+	// clientクラスのインスタンスを作成
+	c := mqtt.NewClient(opts)
+	// Brokerへのconnection及び、Errorがないか判定
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("Mqtt error: %s", token.Error())
+	}
+	// subscribeするtopicが、正しく設定されているか判定
+	if subscribeToken := c.Subscribe("go-mqtt/sample", 0, f); subscribeToken.Wait() && subscribeToken.Error() != nil {
+		log.Fatal(subscribeToken.Error())
+	}
+
+	// systemcallを受け取るchanenlの作成
+	signalCh := make(chan os.Signal, 1)
+	// systemcallがあると知らせる
+	signal.Notify(signalCh, os.Interrupt)
+	// forever
+	for {
+		select {
+		// メッセージをchanellから受信
+		case m := <-msgCh:
+			// println(opts.ClientID)
+			// fmt.Printf("topic: %v, payload: %v\n", m.Topic(), string(m.Payload()))
+			file_name := string(m.Payload())
+			println(file_name)
+			data, err := os.ReadFile(string(file_name))
+			if err != nil {
+				log.Fatal(err)
+			}
+			println(string(data))
+		// systemcallがあると知らせる
+		case <-signalCh:
+			fmt.Printf("Interrupt detected.\n")
+			c.Disconnect(1000)
+			return
+		}
+	}
+}
