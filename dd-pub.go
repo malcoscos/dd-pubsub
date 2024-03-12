c5a9e1bd1f79add901864c32efd8356194da212c
malcoscos <soma.nakagawa.77@gmail.com>
2024-03-12 12:36:19 +0900

    store movie data to local, get movie data using websocket

--- dd-pub.go
+++ dd-pub.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/google/uuid"
@@ -17,65 +19,80 @@ import (
 func Publish(p *PubArg) {
 	var data_mime_type string = ProcessFile(p.Payload)
 
-	// configure minio addr and auth
-	database_addr := fmt.Sprintf("%s:%s", p.StrageAddr, p.StragePort)
-	useSSL := false
-
-	// create minio client
-	var minioClient *minio.Client
-	var err error
-	if p.StrageId != "" {
-		minioClient, err = minio.New(database_addr, &minio.Options{
-			Creds:  credentials.NewStaticV4(p.StrageId, p.StrageKey, ""),
-			Secure: useSSL,
-		})
-		if err != nil {
-			log.Fatalln(err)
-		}
-	} else {
-		minioClient, err = minio.New(database_addr, &minio.Options{
-			Creds:  credentials.Anonymous(),
-			Secure: useSSL,
-		})
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
-
-	// upload data to minio
-	bucket_name := p.Topic
-	exists, err := minioClient.BucketExists(ctx, bucket_name)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	if !exists {
-		err = minioClient.MakeBucket(context.Background(), bucket_name, minio.MakeBucketOptions{})
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
 	object_name := uuid.NewString()
-	payload_data, ok := p.Payload.([]byte)
-	if !ok {
-		return
-	}
-	var reader io.Reader
-	if data, ok := p.Payload.([]byte); ok {
-		reader = bytes.NewReader(data)
-	} else {
-		log.Fatalln("Payload is not of type []byte")
-	}
-	info, err := minioClient.PutObject(context.Background(), bucket_name, object_name, reader, int64(len(payload_data)), minio.PutObjectOptions{})
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Printf("Successfully uploaded %s of size %d\n", object_name, info.Size)
 
-	// get timestamp
+	if data_mime_type == "video" {
+
+		// データをファイルシステムに保存
+		dir := "./saved_files"
+		payload_data, ok := p.Payload.([]byte)
+		if !ok {
+			return
+		}
+		// データをファイルに保存し、ファイルパスを取得
+		filepath, err := saveDataToFile(payload_data, dir, object_name)
+		if err != nil {
+			fmt.Println("Failed to save data to file:", err)
+			return
+		}
+		fmt.Println("Data saved to file:", filepath)
+		object_name = filepath
+
+	} else if data_mime_type == "image" || data_mime_type == "tiny_data" {
+		// configure minio addr and auth
+		database_addr := fmt.Sprintf("%s:%s", p.StrageAddr, p.StragePort)
+		useSSL := false //recommend to change to true in the production env
+		// create minio client
+		var minioClient *minio.Client
+		var err error
+		if p.StrageId != "" {
+			// authenticated communication
+			minioClient, err = minio.New(database_addr, &minio.Options{
+				Creds:  credentials.NewStaticV4(p.StrageId, p.StrageKey, ""),
+				Secure: useSSL,
+			})
+			if err != nil {
+				log.Fatalln(err)
+			}
+		} else {
+			// anonymous communication
+			minioClient, err = minio.New(database_addr, &minio.Options{
+				Creds:  credentials.NewIAM(""),
+				Secure: useSSL,
+			})
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+		// upload data to minio
+		bucket_name := p.Topic
+		exists, err := minioClient.BucketExists(ctx, bucket_name)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if !exists {
+			err = minioClient.MakeBucket(context.Background(), bucket_name, minio.MakeBucketOptions{})
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+
+		var reader io.Reader
+		if data, ok := p.Payload.([]byte); ok {
+			reader = bytes.NewReader(data)
+		} else {
+			log.Fatalln("Payload is not of type []byte")
+		}
+		info, err := minioClient.PutObject(context.Background(), bucket_name, object_name, reader, int64(len(payload_data)), minio.PutObjectOptions{})
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Printf("Successfully uploaded %s of size %d\n", object_name, info.Size)
+	}
+
+	// descriptor of real data
 	now := time.Now()
 	time_stamp := fmt.Sprint(now.Format(time.RFC3339))
-
-	// info of data
 	descriptor := Descriptor{
 		Topic:        p.Topic,
 		DatabaseAddr: p.StrageAddr,
@@ -97,4 +114,36 @@ func Publish(p *PubArg) {
 	token := p.MqttClient.Publish(p.Topic, p.Qos, p.Retained, jsonData)
 	token.Wait()
 	fmt.Println("Complete publish")
+}
+
+func store_tiny_data() {
+
+}
+
+func store_video_data() {
+
+}
+
+func saveDataToFile(data []byte, dir, file_name string) (string, error) {
+	// ディレクトリを作成（存在しない場合）
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return "", err
+	}
+
+	// ファイル名を生成（プレフィックス+タイムスタンプ）
+	fullPath := filepath.Join(dir, file_name)
+
+	// ファイルを開く（存在しない場合は作成、存在する場合は上書き）
+	file, err := os.OpenFile(fullPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	// データをファイルに書き込む
+	if _, err = file.Write(data); err != nil {
+		return "", err
+	}
+
+	return fullPath, nil
 }
--- judge_mime.go
+++ judge_mime.go
@@ -8,16 +8,29 @@ import (
 func ProcessFile(data interface{}) string {
 	bytes, ok := data.([]byte)
 	if !ok {
-		return "unstructured_data"
+		return "tiny_data"
 	}
 
 	mimeType := http.DetectContentType(bytes)
 
 	switch {
 	case mimeType == "video/mp4" || mimeType == "video/x-msvideo":
-		fmt.Println("Processing video file with ffmpeg...")
-		// ここでffmpegを実行します。
-		// 実際にはffmpegコマンドはファイルを要求するため、ファイルへの書き出しが必要です。
+
+		// fmt.Println("Processing video file with ffmpeg...")
+		// // 変換するビデオファイルのパス
+		// inputFile := "input.mp4"
+		// outputFile := "output.avi"
+
+		// // FFmpegコマンドを構築
+		// cmd := exec.Command("ffmpeg", "-i", inputFile, outputFile)
+
+		// // コマンドを実行
+		// if err := cmd.Run(); err != nil {
+		// fmt.Println("Error executing FFmpeg command:", err)
+		// return
+		// }
+		// fmt.Println("Conversion complete, file saved as:", outputFile)
+
 		return "video_data"
 	case mimeType == "image/jpeg" || mimeType == "image/png":
 		fmt.Println("Processing image file with jhead...")
